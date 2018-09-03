a16c47adf3e8be30247ae29efb03adec7463e0ef
honghengqiang <honghengqiang@gmail.com>
2018-09-03 14:46:38 +0800

    xiugai

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 
-app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
+def create_app():
+    app = Flask(__name__)
+    return app
+
+
+app = create_app()
+
 if __name__ == '__main__':
-    app.run(port=8000,host='0.0.0.0')
+    app = create_app()
+    app.run(port=8000, host='0.0.0.0')
